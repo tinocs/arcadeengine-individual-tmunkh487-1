fb82dbbdac405fa61db566a89260efc533d66186
Tushig Munkh <tmunkh487@student.fuhsd.org>
2026-04-09 13:49:02 -0700

    commit

--- P1_Munkh_Tushig_ArcadeEngine/src/engine/World.java
+++ P1_Munkh_Tushig_ArcadeEngine/src/engine/World.java
@@ -5,15 +5,33 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public abstract class World extends Pane {
 
     private AnimationTimer myAnimationTimer;
     private boolean isTimerRunning;
-    private ArrayList<KeyCode> keysPressed;
+    private Set<KeyCode> keysPressed;
     private boolean isWidthInit;
     private boolean isHeightInit;
+    private Set<Actor> actors;
+
+    public World() {
+        keysPressed = new HashSet<>();
+        isWidthInit = false;
+        isHeightInit = false;
+        actors = new HashSet<>();
+        myAnimationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+
+            }
+        };
+
+        widthProperty().addLis
+    }
 
     public abstract void act(long now);
 
@@ -21,8 +39,32 @@ public abstract class World extends Pane {
 
     }
 
-    List<Actor> getObjects(Class<A> cls) {
+    public <A extends Actor> List<A> getObjects(Class<A> cls) {
+        List<A> objects = new ArrayList<>();
+        for (Actor actor : actors) {
+            if (cls.isInstance(actor)) {
+                objects.add(cls.cast(actor));
+            }
+        }
 
+        return objects;
+    }
+
+    public <A extends Actor> List<A> getObjectsAt(double x, double y,  Class<A> cls) {
+        List<A> objects = new ArrayList<>();
+        for (Actor actor : actors) {
+            if (cls.isInstance(actor)) {
+                int minX = actor.getX() - actor.getWidth() / 2;
+                int maxX = actor.getX() + actor.getWidth() / 2;
+                int minY = actor.getY() - actor.getHeight() / 2;
+                int maxY = actor.getY() + actor.getHeight() / 2;
+                if (minX <= x && x <= maxX && minY <= y && y <= maxY) {
+                    objects.add(cls.cast(actor));
+                }
+            }
+        }
+
+        return objects;
     }
 
     public boolean isKeyPressed(KeyCode code) {
